91363c6d3da7add12bec260c752b6335264268d1
markhudsonn <markhudsonn4@gmail.com>
2023-12-11 11:13:19 +0000

    Added icon to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,10 @@
+import { FaGithub} from "react-icons/fa";
+import { GoRepoForked } from "react-icons/go";
+
 function Footer() {
     return (
       <footer className="flex justify-center items-center h-16 text-primary">
+        <GoRepoForked className="w-5 h-5" />
         <p>Portfolio forked from <a className="underline" href="https://b-r.io/">Brian Ruiz</a></p>
       </footer>
     );
